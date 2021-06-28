a91eb76607ce52065da20ea8d1672fad014fe015
DaSheaJ <dashea.johnson.1@gmail.com>
2021-06-27 21:18:25 -0500

    initial commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,11 +17,11 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+             * DONE-Create an abstract class called Vehicle
+             * DONE-The vehicle class shall have three string properties Year, Make, and Model
+             * DONE-Set the defaults to something generic in the Vehicle class
+             * DONE-Vehicle shall have an abstract method called DriveAbstract with no implementation
+             * DONE-Vehicle shall have a virtual method called DriveVirtual with a base implementation.
              */
 
             /* 
@@ -32,17 +32,34 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            Car focus = new Car() { HasTrunk = true, Make = "Ford", Model = "Focus", Year = 2013};
+            Motorcyle motorcycle = new Motorcyle() { HasSideCart = true, Make = "HD", Model = "Chopper", Year = 1978 };
+
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Dodge", Model = "Charger", Year = 2019 };
+            Vehicle sport = new Car() { HasTrunk = false, Make = "Ferari", Model = "F8 Spider", Year = 2020 };
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
+
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine("---------------------------------");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
